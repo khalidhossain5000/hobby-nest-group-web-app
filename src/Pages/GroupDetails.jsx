b5891cda46ce976fa69fd616c6145971f2fd7b82
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-05-20 17:16:12 +0600

    group details page updated

--- src/Pages/GroupDetails.jsx
+++ src/Pages/GroupDetails.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
+import OtherPageNavBar from "../Compoents/Header/OtherPageNavBar";
 
 const GroupDetails = () => {
-      const {groupName,category,description,meetingLocation,maxNumber,imageUrl,startDate}=useLoaderData()
-    return (
-        <div>
-            <h2>{groupName}</h2>
+  const {
+    groupName,
+    category,
+    description,
+    meetingLocation,
+    maxNumber,
+    imageUrl,
+    startDate,
+  } = useLoaderData();
+  return (
+    <div className="">
+        <header>
+            <OtherPageNavBar/>
+        </header>
+      <div className="container mx-auto py-24">
+        <div className="title-img text-center ">
+          <img className="w-7/12 h-2/12 mx-auto" src={imageUrl} alt="" />
+          <h2 className="py-12 text-3xl font-bold text-black">{groupName}</h2>
+          <p>{description}</p>
         </div>
-    );
+        <div className="content text-center">
+          <div>
+            <h3>{category}</h3>
+            <h3>{meetingLocation}</h3>
+          </div>
+          <div>
+            <h3>{maxNumber}</h3>
+            <h3>{startDate}</h3>
+          </div>
+        </div>
+        <button className="w-full text-3xl font-bold btn btn-warning text-black my-6">Join Group</button>
+      </div>
+    </div>
+  );
 };
 
 export default GroupDetails;
