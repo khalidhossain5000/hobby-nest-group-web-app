b484eeea8f7eaa462c6693d8687a2a371ee9f30f
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-05-21 23:24:35 +0600

    slider updated

--- src/Compoents/Slider/Slider.jsx
+++ src/Compoents/Slider/Slider.jsx
@@ -28,8 +28,8 @@ const Slider = () => {
                 <img className="w-full h-[100vh] lg:max-h-[100vh]" src={sliderImg2} alt="" />
                 {/* text and black overlay */}
                 <div className="absolute inset-0 bg-[#00000091] flex flex-col justify-center items-center text-white text-center p-6">
-                    <h2>Discover Local Groups That Share Your Interests</h2>
-                    <h5>Sub title goes here</h5>
+                    <h2 className="text-6xl font-bold text-[#fef0ff]">Find Your People With Hobby Hub</h2>
+                    <h5 className="text-xl font-medium text-[#fef0ff] w-6/12 mx-auto mt-6"> Hey looking for your passion related Group and your passion related People,We are here to help you to find it.Explore Our Website for more information.</h5>
                 </div>
             </div>
         </SwiperSlide>         
@@ -39,8 +39,8 @@ const Slider = () => {
                 <img className="w-full h-[100vh] lg:max-h-[100vh]" src={sliderImg3} alt="" />
                 {/* text and black overlay */}
                 <div className="absolute inset-0 bg-[#00000091] flex flex-col justify-center items-center text-white text-center p-6">
-                    <h2>Discover Local Groups That Share Your Interests</h2>
-                    <h5>Sub title goes here</h5>
+                    <h2 className="text-6xl font-bold text-[#fef0ff]">Turn Your Hobbies Into FriendShip!</h2>
+                    <h5 className="text-xl font-medium text-[#fef0ff] w-6/12 mx-auto mt-6">Using our website you can create your hobbie group and find your related passion people.</h5>
                 </div>
             </div>
         </SwiperSlide>         
@@ -50,8 +50,8 @@ const Slider = () => {
                 <img className="w-full h-[100vh] lg:max-h-[100vh]" src={sliderImg1} alt="" />
                 {/* text and black overlay */}
                 <div className="absolute inset-0 bg-[#00000091] flex flex-col justify-center items-center text-white text-center p-6">
-                    <h2>Discover Local Groups That Share Your Interests</h2>
-                    <h5>Sub title goes here</h5>
+                    <h2 className="text-6xl font-bold text-[#fef0ff]">Your Next Hobbie Adventure Start Here With Us</h2>
+                    <h5 className="text-xl font-medium text-[#fef0ff] w-6/12 mx-auto mt-6">Create account and join our website and find your hobbie realted people and start your next adventure.Start Today!</h5>
                 </div>
             </div>
         </SwiperSlide> 
