66a9701ec4a6cdae0fc96559d011994118aaa5fc
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-05-21 12:23:12 +0600

    create group form design updated

--- src/Pages/CreateGroup.jsx
+++ src/Pages/CreateGroup.jsx
@@ -1,9 +1,11 @@
 import React, { use, useState } from "react";
-import OtherPageNavBar from "../Compoents/Header/OtherPageNavBar";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { AuthContext } from "../Provider/AuthContext";
+// import bgImg from '../assets/groupBg/evening-ii.jpg'
 
+import bgImg from '../assets/groupBg/nmntan-ik.jpg'
+import NavBar from "../Compoents/Header/NavBar";
 const CreateGroup = () => {
   const [startDate, setStartDate] = useState(null);
   const { user } = use(AuthContext);
@@ -43,32 +45,35 @@ const CreateGroup = () => {
   };
 
   return (
-    <div>
+    <div className="">
       <header className="">
-        <OtherPageNavBar></OtherPageNavBar>
+        <NavBar/>
       </header>
-      <main className="container mx-auto">
-        <h1 className="text-center text-6xl font-bold p-12">Create Group</h1>
+      <main style={{backgroundImage:`url(${bgImg})`}} className="bg-cover bg-no-repeat py-36 bg-top-left">
+        <h1 className="text-[#feedff] container mx-auto text-center text-6xl font-bold py-12">Create Group</h1>
+        <h3 className="text-[#feedff] text-center text-3xl font-medium">Fill Up The Form And Create Your Hobbie Group Now!</h3>
         <section className="form-full p-24 ">
           <form onSubmit={handleCreateGroup}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Group Name</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Group Name</label>
                 <input
                   type="text"
                   name="groupName"
-                  className="input w-full"
-                  placeholder="Group Name"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  placeholder="Enter Your Group Name"
                 />
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Select Hobby Category</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Select Hobby Category</label>
                 <select
                   name="category"
-                  defaultValue="Hobby Category"
-                  className="w-full select select-neutral"
+                  
+                  
+                  className="w-full shadow-2xl bg-transparent text-black font-semibold border-2 border-pink-500 py-3 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
                 >
-                  <option disabled={true}>Hobby Category</option>
+                  {/* <option disabled={true}>Hobby Category</option> */}
+                  <option value="" hidden>Select Hobby Category</option>
                   <option>Drawing & Painting</option>
                   <option>Photography</option>
                   <option>Video Gaming</option>
@@ -79,82 +84,83 @@ const CreateGroup = () => {
                   <option>Writing </option>
                 </select>
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Description</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Description</label>
                 <textarea
                   name="description"
                   placeholder="Description"
-                  className="w-full textarea textarea-xs"
+                  className="w-full textarea textarea-xs input shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  
                 ></textarea>
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Meeting Location</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Meeting Location</label>
                 <input
                   type="text"
                   name="meetingLocation"
-                  className="input w-full"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
                   defaultValue="enter a meeting locaitons"
                   placeholder="Meeting Location"
                 />
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Max Members</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Max Members</label>
                 <input
                   type="number"
                   name="maxNumber"
-                  className="input w-full"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
                   placeholder="Max Members"
                 />
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">
                   Start Date (deadline equivalent)
                 </label>
 
-                <div className="border h-6 p-1 rounded">
+                <fieldset className="bg-transparent fieldset rounded-box border border-pink-300">
                   <DatePicker
                     selected={startDate}
                     onChange={(date) => setStartDate(date)}
                     minDate={new Date()}
                     placeholderText="Select a date between today and 5 days in the future"
+                    className="w-full py-3 text-white px-5gi text-xl font-bold"
                   />
-                </div>
+                </fieldset>
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">Image URL</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">Image URL</label>
                 <input
                   type="text"
                   name="imageUrl"
-                  className="input w-full"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
                   placeholder="Image URL"
                 />
               </fieldset>
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">User Name (readonly)</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">User Name (readonly)</label>
                 <input
                   type="text"
-                  name="user-name"
-                  className="input w-full"
-                  
-                  disabled
+                  name="userName"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  readOnly
                   value={`User Name : ${user?.displayName}`}
                 />
               </fieldset>
             </div>
             <div className="py-6">
-              <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-                <label className="label">User Email (readonly)</label>
+              <fieldset className="bg-transparent fieldset rounded-box border border-pink-300 p-4">
+                <label className="label text-pink-100 text-2xl pb-3">User Email (readonly)</label>
                 <input
                   type="text"
-                  name="email-n"
-                  className="input w-full"
-                  disabled
+                  name="email"
+                  className="input w-full shadow-2xl bg-transparent text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  readOnly
                   value={`User Name : ${user?.email || ''}`}
                 />
               </fieldset>
             </div>
 
-            <input type="submit" className="btn w-full" value="Create Group" />
+            <input type="submit" className="border-3 backdrop-blur-xl  border-cyan-600 w-full bg-transparent p-6 text-2xl font-bold cursor-pointer text-pink-100 rounded-xl mt-4 hover:bg-gradient-to-tl hover:from-[#07233c] hover:via-[#1cc0de] hover:to-[#020611] transition duration-500" value="Create Group" />
           </form>
         </section>
       </main>
