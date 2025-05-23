88da0ca45b738c34605d638c4b67caff24a0bb61
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-05-23 12:09:30 +0600

    create group page responsive added

--- src/Pages/AllGroup.jsx
+++ src/Pages/AllGroup.jsx
@@ -13,8 +13,8 @@ const AllGroup = () => {
         <OtherPageNavBar></OtherPageNavBar>
       </header>
       <main className="bg-gradient-to-br from-[#0d0518] via-[#3c0648] to-[#600e8c]">
-        <h1 className="text-[#feedff] container mx-auto text-center text-6xl font-bold py-12">All Group</h1>
-        <section className="container mx-auto form-full p-24 ">
+        <h1 className="text-[#feedff] container mx-auto text-center text-3xl lg:text-6xl font-bold py-12">All Group</h1>
+        <section className="container mx-auto form-full p-6 lg:p-24 ">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {
                 allGroupData.map((group)=><AllGroupCard
